abdda1d8ad949402c911db8ab93f31bd9a3817f5
wicked hacker <33594992+wkdhkr@users.noreply.github.com>
2018-01-22 00:54:12 +0900

    log4js config

--- src/helpers/LoggerHelper.js
+++ src/helpers/LoggerHelper.js
@@ -3,6 +3,9 @@ import log4js from "log4js";
 import type { Logger } from "log4js";
 
 export default class LoggerHelper {
+  static configure(config: Object) {
+    log4js.configure(config);
+  }
   static getLogger(clazz: Object): Logger {
     const logger = log4js.getLogger(`${clazz.constructor.name}`);
     return logger;
