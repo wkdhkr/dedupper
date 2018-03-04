e6da9cc3268b9374b706f1b1f352bc1ba4c27026
wicked hacker <33594992+wkdhkr@users.noreply.github.com>
2018-03-04 12:11:26 +0900

    LoggerHelper test fix

--- __tests__/helpers/LoggerHelper.test.js
+++ __tests__/helpers/LoggerHelper.test.js
@@ -9,17 +9,19 @@ describe(Subject.name, () => {
   const loadSubject = async () =>
     (await import("../../src/helpers/LoggerHelper")).default;
 
-  it("loadUserConfig test", async () => {
+  it("configure, flush, getLogger", async () => {
     jest.doMock("./../../src/helpers/EnvironmentHelper", () => ({
       isTest: () => false
     }));
     jest.doMock("log4js", () => ({
       configure: () => {},
-      shutdown: cb => cb()
+      shutdown: cb => cb(),
+      getLogger: () => ({})
     }));
     const subject = await loadSubject();
 
     expect(subject.configure({})).toBeUndefined();
+    expect(subject.getLogger(Subject)).toEqual({});
     expect(await subject.flush()).toBeUndefined();
   });
 });
