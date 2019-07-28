ce8c7974fcec0c31d07b9490b00331433dbd0cde
wicked hacker <33594992+wkdhkr@users.noreply.github.com>
2019-07-28 16:35:56 +0900

    fix floating point test

--- __tests__/services/deepLearning/NsfwJsService.test.js
+++ __tests__/services/deepLearning/NsfwJsService.test.js
@@ -2,6 +2,9 @@
 
 import TestHelper from "../../../src/helpers/TestHelper";
 
+const closeTo = (expected, precision = 2) => ({
+  asymmetricMatch: actual => Math.abs(expected - actual) < 10 ** -precision / 2
+});
 jest.setTimeout(120000);
 describe("NsfwJsService", () => {
   let config;
@@ -24,11 +27,14 @@ describe("NsfwJsService", () => {
       expect(
         await subject.predict(TestHelper.sampleFile.image.png.anime)
       ).toEqual([
-        { className: "Drawing", probability: 0.9298499822616577 },
-        { className: "Hentai", probability: 0.06746610254049301 },
-        { className: "Neutral", probability: 0.0018833467038348317 },
-        { className: "Porn", probability: 0.0007759135914966464 },
-        { className: "Sexy", probability: 0.00002476646659488324 }
+        { className: "Drawing", probability: closeTo(0.9298499822616577, 4) },
+        { className: "Hentai", probability: closeTo(0.06746610254049301, 4) },
+        {
+          className: "Neutral",
+          probability: closeTo(0.0018833467038348317, 5)
+        },
+        { className: "Porn", probability: closeTo(0.0007759135914966464, 4) },
+        { className: "Sexy", probability: closeTo(0.00002476646659488324, 4) }
       ]);
       end = new Date() - start;
       console.log(`execution time 1: ${end}ms`);
@@ -36,11 +42,11 @@ describe("NsfwJsService", () => {
       expect(
         await subject.predict(TestHelper.sampleFile.image.jpg.default)
       ).toEqual([
-        { className: "Neutral", probability: 0.9606342315673828 },
-        { className: "Drawing", probability: 0.03659099340438843 },
-        { className: "Hentai", probability: 0.00224288715980947 },
-        { className: "Porn", probability: 0.0004970461595803499 },
-        { className: "Sexy", probability: 0.00003508067311486229 }
+        { className: "Neutral", probability: closeTo(0.9606342315673828, 4) },
+        { className: "Drawing", probability: closeTo(0.03659099340438843, 4) },
+        { className: "Hentai", probability: closeTo(0.00224288715980947, 4) },
+        { className: "Porn", probability: closeTo(0.0004970461595803499, 4) },
+        { className: "Sexy", probability: closeTo(0.00003508067311486229, 4) }
       ]);
       end = new Date() - start;
       console.log(`execution time 2: ${end}ms`);
